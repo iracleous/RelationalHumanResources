5bbfa16c89a6a887dbe541e5bc0e814a2acef92c
dimitris <iracleous@gmail.com>
2024-01-12 11:31:46 +0200

    change in the constants

--- RelationalHumanResources/Models/Constants.cs
+++ RelationalHumanResources/Models/Constants.cs
@@ -2,6 +2,7 @@
 {
     public class Constants
     {
-        public static decimal BasicSalary { get; } = 800;
+        public static  decimal BasicSalary { get; } = 800;
+        public static decimal PercentageVat { get; } = 0.24m;
     }
 }
